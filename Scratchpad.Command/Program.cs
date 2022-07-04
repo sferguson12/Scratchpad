a5b590d19cccfe44eda281129bf31f1537bc66e4
Steve Ferguson <sfergus1@gmail.com>
2022-07-04 13:49:49 -0400

    Initial template code

---
+++ Scratchpad.Command/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scratchpad.Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
