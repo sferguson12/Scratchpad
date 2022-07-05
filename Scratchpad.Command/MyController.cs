e37bf478a2d9a3b1ec6fd1b7bcbb6be2fc678782
Steve Ferguson <sfergus1@gmail.com>
2022-07-05 12:35:39 -0400

    Add logging instances

--- Scratchpad.Command/MyController.cs
+++ Scratchpad.Command/MyController.cs
@@ -1,19 +1,24 @@
+using Microsoft.Extensions.Logging;
 using Scratchpad.Library;
 
 namespace Scratchpad.Command
 {
     public class MyController
     {
+        private readonly ILogger _logger;
         private readonly IMyService _myService;
 
         public MyController(
+            ILogger<MyController> logger,
             IMyService myService)
         {
+            _logger = logger;
             _myService = myService;
         }
 
         public void DoSomething()
         {
+            _logger.LogDebug("Invoked DoSomething");
             var result = _myService.DoTest();
 
         }
--- Scratchpad.Library/MyService.cs
+++ Scratchpad.Library/MyService.cs
@@ -1,10 +1,22 @@
+using System;
+using Microsoft.Extensions.Logging;
+
 namespace Scratchpad.Library
 {
     public class MyService : IMyService
     {
+        private readonly ILogger _logger;
+
+        public MyService(
+            ILogger<MyService> logger)
+        {
+            _logger = logger;
+        }
+
         public int DoTest()
         {
-            throw new System.NotImplementedException();
+            _logger.LogDebug("Invoked DoTest");
+            throw new NotImplementedException();
         }
     }
 }
